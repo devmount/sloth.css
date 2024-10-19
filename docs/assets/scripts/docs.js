a4010ab989058414fa44c9f78b51a39aa54492c5
Andreas Müller <mail@devmount.de>
2024-10-19 14:22:03 +0200

    ➕ Theme mode switcher

--- docs/assets/scripts/docs.js
+++ docs/assets/scripts/docs.js
@@ -1,6 +1,17 @@
 // Theming
+const lightToggle = document.querySelector('#light-mode');
+const darkToggle = document.querySelector('#dark-mode');
 const setDark = () => {
-  // document.style.colorScheme = 'dark';
+  localStorage.setItem('sloth.css/theme', 'dark');
+  document.documentElement.style.colorScheme = 'dark';
+  darkToggle.classList.add(['text-accent']);
+  lightToggle.classList.remove(['text-accent']);
+};
+const setLight = () => {
+  localStorage.setItem('sloth.css/theme', 'light');
+  document.documentElement.style.colorScheme = 'light';
+  lightToggle.classList.add(['text-accent']);
+  darkToggle.classList.remove(['text-accent']);
 };
 
 // Component: Toast
@@ -16,3 +27,16 @@ const colorToast = (col) => {
   toastColors.forEach(c => toast.classList.remove(c));
   toast.classList.add(col);
 }
+
+// Ready? Let's go!
+(() => {
+  // Handle initial theme mode
+  if (localStorage.getItem('sloth.css/theme') === 'dark') {
+    document.documentElement.style.colorScheme = 'dark';
+    darkToggle.classList.add('text-accent');
+  }
+  if (localStorage.getItem('sloth.css/theme') === 'light') {
+    document.documentElement.style.colorScheme = 'light';
+    lightToggle.classList.add('text-accent');
+  }
+})();
